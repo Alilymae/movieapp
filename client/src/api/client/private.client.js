c61bea2a259460915edc77ebe90689a0c4843dc4
Aliah Mae <ortizaliah88@gmail.com>
2026-01-17 07:10:06 +0400

    commit ocho

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "http://localhost:5000/api/v1";
+// PRIVATE CLIENT
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1";
 
 const privateClient = axios.create({
     baseURL,
